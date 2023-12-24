d5ebb8320ee29d6483eb4292c832b35e9ab810f5
Hopkins <>
2023-12-24 20:36:40 +0800

    Disable nvim tree launch on startup

--- .config/nvim/lua/core/plugins/nvim-tree.lua
+++ .config/nvim/lua/core/plugins/nvim-tree.lua
@@ -1,15 +1,17 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup()
--- Open tree ib start without focus
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = (
-	function()
-		require("nvim-tree.api").tree.toggle({ focus = false })
-	end
-)})
 
-require("nvim-tree.api").tree.toggle_gitignore_filter() 	-- Shows gitignore files too
+-- -- Open tree ib start without focus
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = (
+-- 	function()
+-- 		require("nvim-tree.api").tree.toggle({ focus = false })
+-- 	end
+-- )})
+-- 
+-- require("nvim-tree.api").tree.toggle_gitignore_filter() 	-- Shows gitignore files too
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+
 
 -- Autoclose if only tree is left
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
