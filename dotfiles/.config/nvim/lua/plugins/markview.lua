9e4387df6456e3e16b9461c13491efff694495ad
Hopkins <>
2024-09-17 02:28:47 +0800

    Update nvim delete keybind and add plugin

---
+++ dotfiles/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,27 @@
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+
+        "nvim-tree/nvim-web-devicons"
+    },
+	config = function()
+		require("markview").setup({
+			headings = require("markview.presets").headings.glow_labels,
+			modes = { "n", "i", "no", "c" },
+			hybrid_modes = { "n", "i" },
+
+			-- This is nice to have
+			callbacks = {
+				on_enable = function (_, win)
+					vim.wo[win].conceallevel = 2;
+					vim.wo[win].concealcursor = "nc";
+				end
+			}
+		})
+	end
+}
+
