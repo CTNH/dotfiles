00726e1444c94969c2baa5afa23aec95cb616d46
Hopkins <>
2024-10-18 04:41:08 +0800

    Add yazi config files

---
+++ dotfiles/.config/yazi/init.lua
@@ -0,0 +1,65 @@
+require("relative-motions"):setup({ show_numbers="none", show_motion = true })
+
+-- Custom linemode
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.modified or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+end
+
+-- Show user and group in status bar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	}
+end, 500, Status.RIGHT)
+
+-- Show symlink in status bar
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line {}
+	end
+
+	-- return ui.Line(" " .. h.name)
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = false },
+	persist = "all",
+	desc_format = "full",
+	file_pick_mode = "parent",
+	notify = {
+		enable = true,
+		timeout = 3,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
+
+require("git"):setup()
+
